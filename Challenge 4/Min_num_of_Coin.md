93113d3888334fd9cfd93dc960306d7067676a16
Ramon Geronimo <rgero215@gmail.com>
2019-08-17 17:41:27 -0700

    fix READMEs

--- Challenge 4/Min_num_of_Coin.md
+++ Challenge 4/Min_num_of_Coin.md
@@ -11,3 +11,20 @@ Sample Output: 3
 
 # Run 
 python3 min_num_of_coin.py
+
+# 5 Steps of Dynamic Programming applied to Knapsack Problem
+
+### 1 Identify the subproblems 
+I build an array in range of 0 to n where the indexes are the amount of dollar and the value is the minimum combination of coin we need to make change for the amount
+
+### 2 Guess the first choice 
+Remove coins randomly 
+
+### 3 Recursively define the value of an optimal solution
+We calculate the number of combination with the coin in and out.
+
+###  4 Compute the value of an optimal solution (recurse and memoize)
+After exploring all the possible combination recursibly, the function will stop when it reach the base case.
+
+### 5 Solve original problem by reconstructing the sub-problems 
+Add up the counts for each possibility of a coin in and out.
--- Challenge 4/min_num_of_coin.py
+++ Challenge 4/min_num_of_coin.py
@@ -9,8 +9,16 @@ def min_num_of_coins_for_change(n, denoms):
         for amount in range(len(num_of_coins)):
             if denom <= amount:
                 num_of_coins[amount] = min(num_of_coins[amount], 1 + num_of_coins[amount - denom])
-
+    
     return num_of_coins[n] if num_of_coins[n] != float('inf') else - 1
 
-print('for this input: 7,[1,5,10]')
-print(f'The optimal solution is: {min_num_of_coins_for_change(n, denoms)}  (2 coins of 1, 1 coin of 5)')
+print('for this input: 7,[1,5,10] the output is 3')
+print(f'The optimal solution is: {min_num_of_coins_for_change(n, denoms)}')
+
+a = 5
+print('for this input: 5,[1,5,10] the output is 1')
+print(f'The optimal solution is: {min_num_of_coins_for_change(a, denoms)}')
+
+b = 15
+print('for this input: 15,[1,5,10] the output is 2')
+print(f'The optimal solution is: {min_num_of_coins_for_change(b, denoms)}')
